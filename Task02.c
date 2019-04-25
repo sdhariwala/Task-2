a4b1bec6cd1672ae5ce658c18b46f76d428aa45c
Mohamad Samir Dhariwala <47292016+sdhariwala@users.noreply.github.com>
2019-04-25 18:34:13 +0530

    Add files via upload

---
+++ Task02.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	float c,f;
+	printf("Enter the Temperature in Celsius:");
+	scanf("%f",&c);
+	f=1.8*c+32;
+	
+	printf("Temperature");
+	printf("\n Celsius:%f",c);
+	printf("\n Fahrenheit:%f",f);
+	
+}
